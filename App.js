e41e14fa4eb5f261364db05b5c0a09c360391aaa
batsieba <batisebatekle@gmail.com>
2024-03-25 16:56:52 +0400

    add react navigation

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './navigation';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-red-500">Call Anywhere!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <Navigation/>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
